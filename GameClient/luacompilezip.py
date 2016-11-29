e47aa2c6ed6af3a89ab4881c7e87eb7db9ad9f20
ff78 <ff@ff.local>
2016-11-29 18:39:36 +0800

    zip tools

--- GameClient/luacompilezip.py
+++ GameClient/luacompilezip.py
@@ -1,17 +1,31 @@
-#coding=utf8
+# -*- coding: cp936 -*-
 import os
+import sys
+import shutil
+import json
 import zipfile
-
 workdir             = None
 
-def zip_dir():
-    pass
+def get_dir(*pathes):
+    if pathes:
+        return os.path.join(workdir, *pathes)
+    return workdir
+
+def zip_dir(srcPath, desPath):
+    zipHandle = zipfile.ZipFile(desPath, 'w', zipfile.ZIP_DEFLATED)
+
+    for dirpath, dirs, files in os.walk(srcPath):
+        for filename in files:
+            zipHandle.write(os.path.join(dirpath,filename))
+            print filename+" zip_succeeded"
+
+    zipHandle.close
+
 
 
 if __name__ == '__main__':    
     workdir = os.path.split(os.path.abspath('__file__'))[0]
-    print('workdir%s'%workdir)    
-    _del_sounds()
-    _update_assets()
-    _copy_icon()
-    _copy_thirdpart_files()
+    # workdir += '/src'
+    print('workdir%s'%workdir)
+    zip_dir('src', 'src.zip')
+
